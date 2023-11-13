7e9863cdc610a5075822aef1c9472635b2124f6d
Shouryya Manna <91773311+Shouryya-Manna@users.noreply.github.com>
2023-11-13 22:43:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const secondsEl = document.getElementById("seconds");
+const minutesEl = document.getElementById("minutes");
+const hoursEl = document.getElementById("hours");
+const daysEl = document.getElementById("days");
+
+const Newyear = '1 January 2024';
+
+function countdown() {
+    const newYearsDate = new Date(Newyear);
+    const currentDate = new Date();
+    const second = (newYearsDate - currentDate) / 1000;
+    const seconds = Math.floor(second) % 60;
+    const minutes = Math.floor(second / 60) % 60;
+    const hours = Math.floor(second / 3600) % 24;
+    const days = Math.floor(second / 3600 / 24);
+
+    console.log(days, hours, minutes, seconds);
+
+    secondsEl.innerHTML = formatTime(seconds);
+    minutesEl.innerHTML = formatTime(minutes);
+    hoursEl.innerHTML = formatTime(hours);
+    daysEl.innerHTML = formatTime(days);
+
+
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
+}
+
+setInterval(countdown, 1000);
+
